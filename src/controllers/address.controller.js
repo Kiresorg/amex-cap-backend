3099bfd48057d9213c4abb8dd54414d028327f98
Daniel Liebman <dliebman100@gmail.com>
2021-07-19 15:34:58 -0400

    GET all addresses endpoint works

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,3 +1,15 @@
+const db = require("../sequelize/models");
+const Address = db.Address;
+
 exports.findAll = (req, res) => {
-  res.send('You have reached the controller.');
+  Address.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving addresses.",
+      });
+    });
 };
