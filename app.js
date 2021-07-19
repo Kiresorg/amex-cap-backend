bd68c1ef7c7015ec648eb81a821058008b826b37
Jason Rosario <Jasonrosario@protonmail.com>
2021-07-19 12:36:22 -0400

    got address router working

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Amex CSR API." });
 });
 
+require('./src/routers/address.route')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
---
+++ src/routers/address.route.js
@@ -0,0 +1,31 @@
+module.exports = app => {
+    // const tutorials = require("../controllers/tutorial.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Tutorial
+    // router.post("/", tutorials.create);
+  
+    // Retrieve all Tutorials
+    router.get("/", (req, res) => {
+        res.send('You have reached the router');
+    });
+  
+    // Retrieve all published Tutorials
+    // router.get("/published", tutorials.findAllPublished);
+  
+    // Retrieve a single Tutorial with id
+    // router.get("/:id", tutorials.findOne);
+  
+    // Update a Tutorial with id
+    // router.put("/:id", tutorials.update);
+  
+    // Delete a Tutorial with id
+    // router.delete("/:id", tutorials.delete);
+  
+    // Create a new Tutorial
+    // router.delete("/", tutorials.deleteAll);
+  
+    app.use('/api/address', router);
+  };
+  
