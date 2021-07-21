04f4ca7df31e11d786cd4027735ce540591dddbe
Daniel Liebman <dliebman100@gmail.com>
2021-07-21 13:39:42 -0400

    added deleteAll controller

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -8,8 +8,45 @@ exports.findAll = (req, res) => {
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving addresses.",
+        message: err.message || "Error occurred while retrieving addresses.",
+      });
+    });
+};
+
+// DELETE addressId
+// exports.deleteAddressId = (req, res) => {
+//   // delete storage instance of Address IDs to remove reference from object
+//   const id = req.params.id;
+//   Address.destroy({
+//     where: { id: id }
+//   })
+//     .then((data) => {
+//       if (data !== 1) {
+//         res.send({
+//           message: `Address IDs NOT deleted successfully. Try Again!`,
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message:
+//           err.message || `Error occurred while deleting address IDs ${id}.`,
+//       });
+//     });
+// };
+
+// DELETE all addresses
+exports.deleteAll = (req, res) => {
+  Address.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((data) => {
+      res.send({ message: `${data} deleted successfully.` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Error occurred while deleting all addresses.`,
       });
     });
 };
--- src/routers/address.route.js
+++ src/routers/address.route.js
@@ -1,29 +1,13 @@
-module.exports = app => {
-    const addresses = require("../controllers/address.controller");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    // router.post("/", tutorials.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", addresses.findAll);
-  
-    // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    // router.get("/:id", tutorials.findOne);
-  
-    // Update a Tutorial with id
-    // router.put("/:id", tutorials.update);
-  
-    // Delete a Tutorial with id
-    // router.delete("/:id", tutorials.delete);
-  
-    // Create a new Tutorial
-    // router.delete("/", tutorials.deleteAll);
-  
-    app.use('/api/address', router);
-  };
-  
+module.exports = (app) => {
+  const addresses = require("../controllers/address.controller");
+
+  var router = require("express").Router();
+
+  // GET all addresses
+  router.get("/", addresses.findAll);
+
+  // DELETE address ID
+  router.delete("/", addresses.deleteAll);
+
+  app.use("/api/address", router);
+};
