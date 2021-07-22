3ac51db3a192154c806ab6507fdfaedb364ff36c
dl86644n <dana.lopez@aol.com>
2021-07-22 16:32:40 -0400

    added conditional for searching by id

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -17,7 +17,12 @@ exports.findById = (req, res) => {
   const id = req.params.id
   Customer.findAll({where: {id:id}})
   .then(data => {
-    res.send({message: "Customer has been found", data})
+    if(data.length === 0){
+      res.send({message: "Customer does not exist"})
+    }
+    else{
+      res.send({message: "Customer has been found", data})
+    }
   }).catch(err => {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving customers.",
