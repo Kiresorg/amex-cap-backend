4afa69dc66971246ac6900614b62b652f7c6544d
Moe Ko <moethantkoko212@gmail.com>
2021-07-23 14:11:31 -0400

    fixed address_id issue in controller

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -14,35 +14,14 @@ exports.findAll = (req, res) => {
     });
 };
 
-// exports.create = (req, res) => {
-//   res.send("you have reached the controller");
-// };
-// exports.create = async (req, res) => {
-//   try {
-//     //this will create new customer for User --- if there's an error, nothing catches them
-//     const newCustomer = await Customer.create(req.body);
-
-//     //if successful send status code, message, and request
-//     res.status(201).json({
-//       newCustomer,
-//     });
-//   } catch (error) {
-//     //if unsuccessful send 400 status code, and error message
-//     res.status(500).json({
-//       statusCode: 500,
-//       error: error || "An error has occurred and Address could not be created",
-//     });
-//   }
-// };
-
 exports.create = (req, res) => {
   let first_name = req.body.first_name;
   let middle_name = req.body.middle_name;
-  let last_name = req.body.first_name;
+  let last_name = req.body.last_name;
   let phone = req.body.phone;
   let email = req.body.email;
   let notes = req.body.notes;
-  let addressID = req.body.addressID;
+  let addressID = req.body.addressId;
 
   Customer.create({
     first_name: first_name,
@@ -51,7 +30,7 @@ exports.create = (req, res) => {
     phone: phone,
     email: email,
     notes: notes,
-    addressID: addressID,
+    address_id: addressID,
   })
     .then((result) => {
       res.status(200).send(result);
