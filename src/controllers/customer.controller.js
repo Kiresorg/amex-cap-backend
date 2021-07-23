7f869c84d512535ce44d1419b672c0f4d0e5de95
Jim Liriano <jim.liriano@gmail.com>
2021-07-23 15:03:03 -0400

    tested PUT for customer-edit

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,7 +1,7 @@
 const db = require("../sequelize/models");
 const Customer = db.Customer;
 
-const findAllByCount = (req, res) => {
+exports.findAllByCount = (req, res) => {
   Customer.findAndCountAll({
     where: {},
     limit: Number(req.query.count),
@@ -17,7 +17,7 @@ const findAllByCount = (req, res) => {
     });
 };
 
-const findAll = (req, res) => {
+exports.findAll = (req, res) => {
   Customer.findAll()
     .then((data) => {
       res.send(data);
@@ -31,38 +31,46 @@ const findAll = (req, res) => {
 };
 
 exports.findById = (req, res) => {
-  const id = req.params.id
-  Customer.findAll({where: {id:id}})
-  .then(data => {
-    if(data.length === 0){
-      res.status(404).send({message: "Customer does not exist"})
-    }
-    else{
-      res.send({data})
-    }
-  }).catch(err => {
-    res.status(500).send({
-      message: err.message || "Some error occurred while retrieving customers.",
-      
+  const id = req.params.id;
+  Customer.findAll({ where: { id: id } })
+    .then((data) => {
+      if (data.length === 0) {
+        res.status(404).send({ message: "Customer does not exist" });
+      } else {
+        res.send({ data });
+      }
     })
-  })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving customers.",
+      });
+    });
 };
 
 exports.update = (req, res) => {
-  Customer.update({
-    first_name: req.body.first_name,
-    middle_name: req.body.middle_name,
-    last_name: req.body.last_name,
-    phone: req.body.phone,
-    email: req.body.email,
-    notes: req.body.notes,
-  },{
-    where: {
-      id: req.params.id
+  Customer.update(
+    {
+      first_name: req.body.first_name,
+      middle_name: req.body.middle_name,
+      last_name: req.body.last_name,
+      phone: req.body.phone,
+      email: req.body.email,
+      notes: req.body.notes,
+      address_id: req.body.address_id
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
     }
-  }).then (result => {
+  )
+    .then((result) => {
       res.status(200).send(result);
-  }).catch (error => {
-      res.status(500).send("Couldn't update customer with id of" + req.params.id + ":");
-  })
-}
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send("Couldn't update customer with id of" + req.params.id + ":");
+    });
+};
--- src/routers/customer.route.js
+++ src/routers/customer.route.js
@@ -1,21 +1,14 @@
 module.exports = (app) => {
-  const customerController = require(`../controllers/customer.controller`);
+  const customers = require("../controllers/customer.controller");
 
   const router = require("express").Router();
 
-  // GET ALL CUSTOMERS
-  // router.get('/', customerController.getAll);
-
-  // GET Customer By ID
-  // router.get('/:id', customerController.getById);
-
-  // Update customer by Id
-  router.put("/:id", customerController.update);
-
   router.get("/:id", customers.findById);
 
-  app.use("/api/customers", router);
+  router.put("/:id", customers.update);
 
-  router.get("/all", customers.findAll);
-  router.get("/", customers.findAllByCount);
+  // router.get("/all", customers.findAll);
+  router.get("/", customers.findAll);
+
+  app.use("/api/customers", router);
 };
