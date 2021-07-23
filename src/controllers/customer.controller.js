b62806534bc446264e94ba34b1e71c4560405434
Moe Ko <moethantkoko212@gmail.com>
2021-07-23 09:56:07 -0400

    post request on customers.http works

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -14,6 +14,25 @@ exports.findAll = (req, res) => {
     });
 };
 
-exports.create = (req, res) => {
-  res.send("you have reached the controller");
+// exports.create = (req, res) => {
+//   res.send("you have reached the controller");
+// };
+exports.create = async (req, res) => {
+  try {
+    //this will create new customer for User
+    const newCustomer = await Customer.create(req.body);
+
+    //if successful send status code, message, and request
+    res.status(201).json({
+      statusCode: 201,
+      message: "Customer Has Been Created",
+      newCustomer,
+    });
+  } catch (error) {
+    //if unsuccessful send 400 status code, and error message
+    res.status(500).json({
+      statusCode: 500,
+      error: error || "An error has occurred and Address could not be created",
+    });
+  }
 };
