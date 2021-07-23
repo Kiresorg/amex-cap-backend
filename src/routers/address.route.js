819e0fd5b990c326e0f538c338966602bf0b8d8a
Jason Rosario <Jasonrosario@protonmail.com>
2021-07-23 09:22:45 -0400

    Pulling latest

--- src/routers/address.route.js
+++ src/routers/address.route.js
@@ -1,29 +1,28 @@
-module.exports = app => {
-    const addresses = require("../controllers/address.controller");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    // router.post("/", tutorials.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", addresses.findAll);
-  
-    // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    // router.get("/:id", tutorials.findOne);
-  
-    // Update a Tutorial with id
-    // router.put("/:id", tutorials.update);
-  
-    // Delete a Tutorial with id
-    // router.delete("/:id", tutorials.delete);
-  
-    // Create a new Tutorial
-    // router.delete("/", tutorials.deleteAll);
-  
-    app.use('/api/address', router);
-  };
-  
+module.exports = (app) => {
+  const addresses = require("../controllers/address.controller");
+
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  // router.post("/", tutorials.create);
+
+  // Retrieve all Tutorials
+  router.get("/", addresses.findAll);
+
+  // Retrieve all published Tutorials
+  // router.get("/published", tutorials.findAllPublished);
+
+  // Retrieve a single Tutorial with id
+  // router.get("/:id", tutorials.findOne);
+
+  // Update a Tutorial with id
+  // router.put("/:id", tutorials.update);
+
+  // Delete a Tutorial with id
+  // router.delete("/:id", tutorials.delete);
+
+  // Create a new Tutorial
+  // router.delete("/", tutorials.deleteAll);
+
+  app.use("/api/addresses", router);
+};
