1964d3472167bc80ed63c4f72cd7a2c5da794f29
Moe Ko <moethantkoko212@gmail.com>
2021-07-23 12:18:24 -0400

    re-code customer controller exports.create

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -17,22 +17,46 @@ exports.findAll = (req, res) => {
 // exports.create = (req, res) => {
 //   res.send("you have reached the controller");
 // };
-exports.create = async (req, res) => {
-  try {
-    //this will create new customer for User
-    const newCustomer = await Customer.create(req.body);
+// exports.create = async (req, res) => {
+//   try {
+//     //this will create new customer for User --- if there's an error, nothing catches them
+//     const newCustomer = await Customer.create(req.body);
 
-    //if successful send status code, message, and request
-    res.status(201).json({
-      statusCode: 201,
-      message: "Customer Has Been Created",
-      newCustomer,
+//     //if successful send status code, message, and request
+//     res.status(201).json({
+//       newCustomer,
+//     });
+//   } catch (error) {
+//     //if unsuccessful send 400 status code, and error message
+//     res.status(500).json({
+//       statusCode: 500,
+//       error: error || "An error has occurred and Address could not be created",
+//     });
+//   }
+// };
+
+exports.create = (req, res) => {
+  let first_name = req.body.first_name;
+  let middle_name = req.body.middle_name;
+  let last_name = req.body.first_name;
+  let phone = req.body.phone;
+  let email = req.body.email;
+  let notes = req.body.notes;
+  let addressID = req.body.addressID;
+
+  Customer.create({
+    first_name: first_name,
+    middle_name: middle_name,
+    last_name: last_name,
+    phone: phone,
+    email: email,
+    notes: notes,
+    addressID: addressID,
+  })
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((error) => {
+      res.status(500).send("Error on create address: " + error);
     });
-  } catch (error) {
-    //if unsuccessful send 400 status code, and error message
-    res.status(500).json({
-      statusCode: 500,
-      error: error || "An error has occurred and Address could not be created",
-    });
-  }
 };
