7a5afbca2925a8b023295926f02f19e75ab94ee2
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-07-23 11:56:12 -0400

    changing controller for createAddress feature

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -14,23 +14,14 @@ exports.findAll = (req, res) => {
     });
 };
 
-exports.create = async (req, res) => {
-  try {
+exports.create = (req, res) => {
+
     //this will create new address for User
-    const newAddress = await Address.create(req.body);
-
-    //if successful send status code, message, and request
-    res.status(201).json({
-      statusCode: 201,
-      message: "Address Has Been Created",
-      newAddress
+    Address.create(req.body)
+    .then(data =>{
+      res.status(201).send(data)
     })
-
-  }catch(error){
-    //if unsuccessful send 400 status code, and error message
-    res.status(500).json({
-      statusCode: 500,
-      error: error || "An error has occurred and Address could not be created"
+    .catch(error =>{
+      res.status(500).send({message: error || "An error has occurred and Address could not be created"})
     })
-  }
 }
