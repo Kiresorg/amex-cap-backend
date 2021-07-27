49b4e9b4c2089a80c8fa97647ee55cbae2de0f7d
dl86644n <dana.lopez@aol.com>
2021-07-27 10:02:42 -0400

    back end component completed

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -12,3 +12,14 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+exports.findById = (req, res) => {
+  const id = req.params.id
+  Product.findOne({where: {id:id}})
+  .then((data) => {
+    res.status(200).send(data)
+  })
+  .catch(error => {
+    res.status(500).send({message: "Error grabbing product",})
+  })
+}
