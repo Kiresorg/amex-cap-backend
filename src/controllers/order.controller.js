0bc305c96d3d57bd872684e21acd1637b7a14674
Moe Ko <moethantkoko212@gmail.com>
2021-07-27 10:56:16 -0400

    routed the back end for user story 14

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -12,3 +12,20 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+exports.findById = (req, res) => {
+  const id = req.params.id;
+  Order.findAll({ where: { id: id } })
+    .then((data) => {
+      if (data.length === 0) {
+        res.status(404).send({ message: "Order does not exist" });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving order.",
+      });
+    });
+};
