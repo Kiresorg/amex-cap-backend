ce2a04fd53c4f738376e20b55d71dee0e0984444
Joyce Young <jjyoung145@gmail.com>
2021-07-28 17:53:58 -0400

    added product detail to findAll method

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -7,7 +7,10 @@ const Status = require("../utils/orderstatus");
 
 exports.findAll = (req, res) => {
   Order.findAll({
-    include: [{ model: Customer, required: true }],
+    include: [
+      { model: Customer, required: true },
+      { model: Product, required: true },
+    ],
   })
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
@@ -24,6 +27,30 @@ exports.findAll = (req, res) => {
     });
 };
 
+exports.create = (req, res) => {
+  let customer_id = req.body.customer_id;
+  let order_status = req.body.order_status;
+  let datetime_order_placed = req.body.datetime_order_placed;
+  let total_order_price = req.body.total_order_price;
+  let order_notes = req.body.order_notes;
+  let ProductId = req.body.ProductId;
+
+  Order.create({
+    customer_id,
+    order_status,
+    datetime_order_placed,
+    total_order_price,
+    order_notes,
+    ProductId,
+  })
+    .then((result) => {
+      res.status(201).send(result);
+    })
+    .catch((error) => {
+      res.status(500).send("Error on create order: " + error);
+    });
+};
+
 exports.findById = (req, res) => {
   const id = req.params.id;
   Order.findByPk(id, {
@@ -36,7 +63,7 @@ exports.findById = (req, res) => {
       if (data.length === 0) {
         res.status(404).send({ message: "Order does not exist" });
       } else {
-        res.send(data);
+        res.status(200).send(data);
       }
     })
     .catch((err) => {
