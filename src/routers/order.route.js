a110355dbdbf34ff7e2ddc508dca6cefb9407c7c
tcadette <tcadette@gmail.com>
2021-07-28 09:35:01 -0400

    order controller settings

--- src/routers/order.route.js
+++ src/routers/order.route.js
@@ -1,9 +1,20 @@
 module.exports = (app) => {
-  const orders = require("../controllers/order.controller");
+const orders = require("../controllers/orders.controller");
+  
+    var router = require("express").Router();
+  
+    // Create a new Order
+    router.post("/", orders.create);
+  
+    // Retrieve all Order
+    router.get("/", orders.findAll);
+  
+    // Update an Order with id
+    router.put("/:id", orders.update);
+  
+    // Delete an Order with id
+    router.delete("/:id", orders.delete);
+  
+    app.use('/api/orders', router);
+  };
 
-  const router = require("express").Router();
-
-  router.get("/", orders.findAll);
-
-  app.use("/api/orders", router);
-};
