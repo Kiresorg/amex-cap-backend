014600ee03f4eb44b9864ae9267761ab3508153b
Daniel Liebman <dliebman100@gmail.com>
2021-07-28 14:27:00 -0400

    added new unit tests PUT/GET/POST

--- __tests__/addresses/addresses.test.js
+++ __tests__/addresses/addresses.test.js
@@ -1,27 +1,49 @@
-const app = require('../../server');
-const request = require('supertest')
+const app = require("../../server");
+const request = require("supertest");
 
-describe('GET Endpoints', () => {
-  it('should get all addresses', async () => {
-    const res = await request(app)
-      .get('/api/addresses')
-    expect(res.statusCode).toEqual(200)
-  })
-})
-
-
-describe('POST Endpoints', () => {
-  it('should create an address', async () => {
+// CREATE
+describe("POST Endpoints", () => {
+  it("should create an address", async () => {
     const address = {
       address_line_1: "123 main street",
       address_line_2: "apt 4B",
       city: "Portland",
       state: "OR",
-      zip: "97239"
-    }
-    const res = await request(app)
-      .post('/api/addresses').send(address)
+      zip: "97239",
+    };
+    const res = await request(app).post("/api/addresses").send(address);
 
-    expect(res.statusCode).toEqual(201)
-  })
-})
+    expect(res.statusCode).toEqual(201);
+  });
+});
+
+// READ
+describe("GET Endpoints", () => {
+  it("should get all addresses", async () => {
+    const res = await request(app).get("/api/addresses");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe("GET Endpoints", () => {
+  it("should get all addresses by ID", async () => {
+    const res = await request(app).get("/api/addresses/20");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// UPDATE
+describe("PUT Endpoints", () => {
+  it("should update an address", async () => {
+    const address = {
+      address_line_1: "123 main street",
+      address_line_2: "apt 4B",
+      city: "Portland",
+      state: "OR",
+      zip: "97239",
+    };
+    const res = await request(app).put("/api/addresses/20").send(address);
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
