fd6105f1f2123cd6daa514f6dbce6852d8635ee9
Andre Brown <andreups@msn.com>
2021-07-29 17:24:11 -0400

    adding delete functionality

--- src/routers/order.route.js
+++ src/routers/order.route.js
@@ -6,7 +6,6 @@ module.exports = (app) => {
   router.get("/", orders.findAll);
   router.get("/:id", orders.findById);
   router.post("/", orders.create);
-
   router.delete("/:id", orders.delete);
 
   app.use("/api/orders", router);
