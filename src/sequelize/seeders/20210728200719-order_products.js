10c9dd8e0a5422c5a9d045410721d5564f07cd04
iabonirob <iabonirob@gmail.com>
2021-07-29 16:06:47 -0400

    adjusted the findbyId for customer to pull in information from orders

--- src/sequelize/seeders/20210728200719-order_products.js
+++ src/sequelize/seeders/20210728200719-order_products.js
@@ -2,22 +2,28 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    let orderProducts = [];
-    let orders = await queryInterface.sequelize.query(`SELECT id from Orders;`);
-    let products = await queryInterface.sequelize.query(
-      `SELECT id from Products;`
-    );
+    try {
+      let orderProducts = [];
+      let orders = await queryInterface.sequelize.query(
+        `SELECT id from Orders;`
+      );
+      let products = await queryInterface.sequelize.query(
+        `SELECT id from Products;`
+      );
 
-    for (let i = 0; i <= 100; i++) {
-      orderProducts.push({
-        OrderId: orders[0][Math.floor(Math.random() * orders[0].length)].id,
-        ProductId:
-          products[0][Math.floor(Math.random() * products[0].length)].id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
+      for (let i = 0; i <= 100; i++) {
+        orderProducts.push({
+          OrderId: orders[0][Math.floor(Math.random() * orders[0].length)].id,
+          ProductId:
+            products[0][Math.floor(Math.random() * products[0].length)].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+      await queryInterface.bulkInsert("order_products", orderProducts, {});
+    } catch (error) {
+      console.log(error);
     }
-    await queryInterface.bulkInsert("order_products", orderProducts, {});
   },
 
   down: async (queryInterface, Sequelize) => {
