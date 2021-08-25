eb201ec19db459bbf678f702cd1c665479844b3e
Erik Gross <erik@developintelligence.com>
2021-08-25 11:52:54 -0700

    updated room message with bang

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to our CSR API." });
+  res.json({ message: "Welcome to our CSR API!" });
 });
 require("./src/routers/address.route")(app);
 require("./src/routers/product.route")(app);
