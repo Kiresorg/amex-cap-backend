c17e4fe25959f635a64ace6d2235a48e4aee381c
jlawrence2171 <81828527+jlawrence2171@users.noreply.github.com>
2022-02-02 22:51:23 -0500

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+const app = express();
 app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
